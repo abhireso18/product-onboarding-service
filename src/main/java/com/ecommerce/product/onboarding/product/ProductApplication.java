b8df7adb1fef9458adbc121914c2643e837f4f30
Arthfc-repo <Art.powerbi@Arthfc.com>
2024-05-19 23:44:32 +0530

    Dilip : Initial Test Commit

--- src/main/java/com/ecommerce/product/onboarding/product/ProductApplication.java
+++ src/main/java/com/ecommerce/product/onboarding/product/ProductApplication.java
@@ -1,9 +1,11 @@
 package com.ecommerce.product.onboarding.product;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class ProductApplication {
 
 	public static void main(String[] args) {
